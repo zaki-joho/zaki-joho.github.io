430307061eda5666911e14bcb1950de7c614b9be
Yamazaki Hiroki <yamazaki.hiroki.58r@gmail.com>
2020-11-16 17:20:47 +0900

    add karzanov

---
+++ test/dinic_with_karzanov_test.cpp
@@ -0,0 +1,18 @@
+// http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=GRL_6_A
+
+#include "../cpp/dinic_with_karzanov.hpp"
+
+using namespace std;
+
+int main() {
+  int V, E;
+  cin >> V >> E;
+  dinic<int> flow(V);
+  for (int i = 0; i < E; i++) {
+    int u, v, c;
+    cin >> u >> v >> c;
+    flow.add_edge(u, v, c);
+  }
+  cout << flow.flow(0, V - 1) << endl;
+  return 0;
+}
